fa38be74cd758b96596bee8d211bbc2e51376ae3
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-27 18:52:29 +0300

    wip

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,4 +1,7 @@
-function find() { // Egzersiz A
+const db = require('../../data/db-config')
+
+function find() {
+  // Egzersiz A
   /*
     1A- Aşağıdaki SQL sorgusunu SQLite Studio'da "data/schemes.db3" ile karşılaştırarak inceleyin.
     LEFT joini Inner joine çevirirsek ne olur?
@@ -15,9 +18,17 @@ function find() { // Egzersiz A
     2A- Sorguyu kavradığınızda devam edin ve onu Knex'te oluşturun.
     Bu işlevden elde edilen veri kümesini döndürün.
   */
+
+  return db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('sc.* ')
+    .count('st.step_id as number_of_steps')
+    .groupBy('sc.scheme_id')
+    .orderBy('sc.scheme_id', 'asc')
 }
 
-function findById(scheme_id) { // Egzersiz B
+async function findById(scheme_id) {
+  // Egzersiz B
   /*
     1B- Aşağıdaki SQL sorgusunu SQLite Studio'da "data/schemes.db3" ile karşılaştırarak inceleyin:
 
@@ -83,9 +94,39 @@ function findById(scheme_id) { // Egzersiz B
         "steps": []
       }
   */
+
+  const schemeWithSteps = await db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('sc.scheme_name', 'st.*')
+    .where('sc.scheme_id', scheme_id)
+    .orderBy('st.step_number', 'asc')
+
+  if (schemeWithSteps.length === 0) {
+    return null
+  }
+
+  const responseData = {
+    scheme_id: scheme_id,
+    scheme_name: [schemeWithSteps].scheme_name,
+    steps: [],
+  }
+
+  if ([schemeWithSteps].step_id === null) {
+    return responseData
+  }
+
+  for (i = 0; i < schemeWithSteps.length; i++) {
+    responseData.steps.push({
+      step_id: schemeWithSteps[i].step_id,
+      step_number: schemeWithSteps[i].step_number,
+      instructions: schemeWithSteps[i].instructions,
+    })
+  }
+  return responseData
 }
 
-function findSteps(scheme_id) { // Egzersiz C
+function findSteps(scheme_id) {
+  // Egzersiz C
   /*
     1C- Knex'te aşağıdaki verileri döndüren bir sorgu oluşturun.
     Adımlar, adım_numarası'na göre sıralanmalıdır ve dizi
@@ -108,13 +149,15 @@ function findSteps(scheme_id) { // Egzersiz C
   */
 }
 
-function add(scheme) { // Egzersiz D
+function add(scheme) {
+  // Egzersiz D
   /*
     1D- Bu işlev yeni bir şema oluşturur ve _yeni oluşturulan şemaya çözümlenir.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+function addStep(scheme_id, step) {
+  // EXERCISE E
   /*
     1E- Bu işlev, verilen 'scheme_id' ile şemaya bir adım ekler.
     ve verilen "scheme_id"ye ait _tüm adımları_ çözer,
