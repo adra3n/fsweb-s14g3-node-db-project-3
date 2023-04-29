a864bbe113135948917b5df8449170e73f196934
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-29 09:01:04 +0300

    esnek gorevler tamamlandi

--- queries.sql
+++ queries.sql
@@ -39,3 +39,46 @@ ON o.EmployeeId=e.Id
 JOIN Customer AS c
 ON c.Id=o.CustomerId
 
+
+-- Her gönderici tarafından gönderilen gönderi sayısını bulun.
+
+SELECT s.ShipperName as "Gönderici Adı", count(*) AS "Gönderi Sayısı" FROM [Orders] AS o
+JOIN [Shippers] AS s ON o.ShipperID = s.ShipperID
+GROUP BY o.ShipperID
+
+-- Sipariş sayısına göre ölçülen en iyi performans gösteren ilk 5 çalışanı bulun.
+
+SELECT (e.FirstName||' '||e.LastName)  AS "Çalışan Adı", count(*) AS "Toplam Sipariş" FROM [Orders] AS o
+JOIN [Employees] AS e ON e.EmployeeID=o.EmployeeID 
+GROUP BY o.EmployeeID 
+ORDER BY "Toplam Sipariş" DESC
+LIMIT 5
+
+-- Gelir olarak ölçülen en iyi performans gösteren ilk 5 çalışanı bulun.
+
+SELECT (e.FirstName||' '||e.LastName)  AS "Çalışan Adı", SUM(p.Price*od.Quantity) AS "Sipariş Toplamı" FROM [Orders] AS o
+JOIN [OrderDetails] AS od ON o.OrderID= od.OrderID
+JOIN [Employees] AS e ON e.EmployeeID = o.EmployeeID
+JOIN [Products] AS p ON od.ProductID = p.ProductID
+GROUP BY e.EmployeeID
+ORDER BY "Sipariş Toplamı" DESC
+LIMIT 5
+
+-- En az gelir getiren kategoriyi bulun.
+
+SELECT c.CategoryName AS "Kategori Adı", SUM(p.Price*od.Quantity) AS "Sipariş Toplamı" FROM [OrderDetails] AS od
+JOIN [Categories] AS c ON c.CategoryID = p.CategoryID
+JOIN [Products] AS p ON od.ProductID = p.ProductID
+GROUP BY c.CategoryID
+ORDER BY "Sipariş Toplamı" ASC
+LIMIT 1
+
+-- En çok siparişi olan müşteri ülkesini bulun.
+
+SELECT  c.Country AS "Ülke" ,count(*) AS "Toplam Sipariş" FROM [Orders] AS o
+JOIN [OrderDetails] AS od ON o.OrderID= od.OrderID
+JOIN [Customers] AS c ON c.CustomerID = o.CustomerID
+GROUP BY c.Country
+ORDER BY "Toplam Sipariş" DESC
+LIMIT 1
+
